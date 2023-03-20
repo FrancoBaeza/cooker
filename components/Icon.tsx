04ab521e4f5cf1d414d0f852da7b172de8852dbe
Franco Baeza Graf <francobaezagraf@gmail.com>
2023-03-20 16:22:40 -0300

    save

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -43,10 +43,19 @@ export default function Icon(props: { icon: string; className?: string }) {
             {icon === "image" && (
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
-                   className={className}
+                    className={className}
                     viewBox="0 0 24 24"
                 >
                     <path d="M14 9l-2.519 4-2.481-1.96-5 6.96h16l-6-9zm8-5v16h-20v-16h20zm2-2h-24v20h24v-20zm-20 6c0-1.104.896-2 2-2s2 .896 2 2c0 1.105-.896 2-2 2s-2-.895-2-2z" />
+                </svg>
+            )}
+            {icon === "add" && (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className={className}
+                    viewBox="0 0 24 24"
+                >
+                    <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z" />
                 </svg>
             )}
         </>
